d8245436891980556d513af89db2aca46689b191
Povilas <167984240+Povilas88@users.noreply.github.com>
2024-08-05 18:43:32 +0300

    btn ternary

--- js/index.js
+++ js/index.js
@@ -4,14 +4,10 @@ const resetDOM = document.querySelectorAll('.resultBtn button')[1]
 let lastClicked = '';
 for (let i = 0; i < boardBtnDOM.length; i++) {
     boardBtnDOM[i].addEventListener('click', () => {
-        if (lastClicked === 'X' && boardBtnDOM[i].textContent.length === 0) {
-            boardBtnDOM[i].textContent = 'O';
-            boardBtnDOM[i].style.color = '#0d6164'
-            lastClicked = 'O';
-        } else if (boardBtnDOM[i].textContent.length === 0) {
-            boardBtnDOM[i].textContent = 'X';
-            boardBtnDOM[i].style.color = '#f0f0f0'
-            lastClicked = 'X';
+        if (boardBtnDOM[i].textContent.length === 0) {
+            boardBtnDOM[i].textContent = lastClicked === 'X' ? 'O' : 'X';
+            boardBtnDOM[i].style.color = lastClicked === 'X' ? '#0d6164' : '#f0f0f0';
+            lastClicked = lastClicked === 'X' ? 'O' : 'X';
         }
     });
 }
@@ -19,5 +15,6 @@ for (let i = 0; i < boardBtnDOM.length; i++) {
 for (let i = 0; i < boardBtnDOM.length; i++) {
     resetDOM.addEventListener('click', () => {
         boardBtnDOM[i].textContent = '';
+        lastClicked = '';
     });
 }
