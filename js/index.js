262331461370031a5f83803038b462c68bc4f4a1
Povilas <167984240+Povilas88@users.noreply.github.com>
2024-08-05 21:37:15 +0300

    checkWin() + draw

--- js/index.js
+++ js/index.js
@@ -1,17 +1,22 @@
 const boardBtnDOM = document.querySelectorAll('.buttonContainer button');
+const playDOM = document.querySelectorAll('.resultBtn button')[0]
 const resetDOM = document.querySelectorAll('.resultBtn button')[1]
+const resultMSG = document.querySelector('.resultContainer p');
 const dot1DOM = document.querySelector('.dot1')
 const dot2DOM = document.querySelector('.dot2')
 
+let eventArray = Array(9).fill(null);
 let lastClicked = '';
 for (let i = 0; i < boardBtnDOM.length; i++) {
     boardBtnDOM[i].addEventListener('click', () => {
-        if (boardBtnDOM[i].textContent.length === 0) {
+        if (!eventArray[i] && boardBtnDOM[i].textContent.length === 0) {
             boardBtnDOM[i].textContent = lastClicked === 'X' ? 'O' : 'X';
             boardBtnDOM[i].style.color = lastClicked === 'X' ? '#0d6164' : '#f0f0f0';
             dot1DOM.style.backgroundColor = lastClicked === 'X' ? '#fec32d' : 'transparent';
             dot2DOM.style.backgroundColor = lastClicked === 'X' ? 'transparent' : '#fec32d';
             lastClicked = lastClicked === 'X' ? 'O' : 'X';
+            eventArray[i] = lastClicked;
+            checkWin();
         }
     });
 }
@@ -22,5 +27,37 @@ for (let i = 0; i < boardBtnDOM.length; i++) {
         lastClicked = '';
         dot1DOM.style.backgroundColor = '#fec32d';
         dot2DOM.style.backgroundColor = 'transparent';
+        eventArray = Array(9).fill(null);
+        resultMSG.innerHTML = '';
     });
 }
+
+const winCondition = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function checkWin() {
+    let draw = true;
+    for (let condition of winCondition) {
+        const [a, b, c] = condition;
+        if (eventArray[a] && eventArray[a] === eventArray[b] && eventArray[a] === eventArray[c]) {
+            resultMSG.innerHTML = `Player ${eventArray[a]} wins!`;
+            draw = false;
+            break;
+        }
+    }
+    if (draw && !eventArray.includes(null)) {
+        resultMSG.innerHTML = `It's a DRAW!`;
+    }
+}
+
+playDOM.addEventListener('click', () => {
+    console.log(eventArray.length);
+});
