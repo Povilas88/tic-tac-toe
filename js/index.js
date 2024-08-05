30cf675d0fed828f4fe41605a58477b14e7aa57f
Povilas <167984240+Povilas88@users.noreply.github.com>
2024-08-05 19:38:42 +0300

    indicator

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const boardBtnDOM = document.querySelectorAll('.buttonContainer button');
 const resetDOM = document.querySelectorAll('.resultBtn button')[1]
+const dot1DOM = document.querySelector('.dot1')
+const dot2DOM = document.querySelector('.dot2')
 
 let lastClicked = '';
 for (let i = 0; i < boardBtnDOM.length; i++) {
@@ -7,6 +9,8 @@ for (let i = 0; i < boardBtnDOM.length; i++) {
         if (boardBtnDOM[i].textContent.length === 0) {
             boardBtnDOM[i].textContent = lastClicked === 'X' ? 'O' : 'X';
             boardBtnDOM[i].style.color = lastClicked === 'X' ? '#0d6164' : '#f0f0f0';
+            dot1DOM.style.backgroundColor = lastClicked === 'X' ? '#fec32d' : 'transparent';
+            dot2DOM.style.backgroundColor = lastClicked === 'X' ? 'transparent' : '#fec32d';
             lastClicked = lastClicked === 'X' ? 'O' : 'X';
         }
     });
@@ -16,5 +20,7 @@ for (let i = 0; i < boardBtnDOM.length; i++) {
     resetDOM.addEventListener('click', () => {
         boardBtnDOM[i].textContent = '';
         lastClicked = '';
+        dot1DOM.style.backgroundColor = '#fec32d';
+        dot2DOM.style.backgroundColor = 'transparent';
     });
 }
